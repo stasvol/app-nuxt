8a83117d32693b6d385b831e309b0885f54600aa
stas <stasv826gmail.com>
2022-06-21 03:19:21 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar/>
+    <main>
+      <div class="container">
+        <Nuxt />
+      </div>
+    </main>
+
+  </div>
+</template>
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  comments: {
+    Navbar
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hello World</h1>
+<!--  <Tutorial/>-->
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
